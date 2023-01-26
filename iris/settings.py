4d1ae70c994017574e35a71f7bcca96acfa4ebf6
Kerriea-star <ojared511@gmail.com>
2023-01-26 08:37:51 +0300

    mostly complete

--- iris/settings.py
+++ iris/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'iris.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'IrisDB',
+        'USER': "postgres",
+        'PASSWORD': "68@259779Ja",
+        "HOST": "127.0.0.1",
+        'PORT': '5432'
     }
 }
 
