7f04cd0b784443e514227f99d1677975714fee6f
Kerriea-star <ojared511@gmail.com>
2023-01-26 09:25:34 +0300

    fixes

--- iris/settings.py
+++ iris/settings.py
@@ -1,5 +1,8 @@
 import os
+import environ
 
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -8,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -65,11 +68,11 @@ WSGI_APPLICATION = 'iris.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': "",
-        'PASSWORD': "",
-        "HOST": "",
-        'PORT': ''
+        'NAME': env('DB_NAME'),
+        'USER': env ("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        'PORT': env('DB_PORT')
     }
 }
 
