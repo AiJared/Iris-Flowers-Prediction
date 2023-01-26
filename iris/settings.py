d2ddb40314fdff64029dd20dcc4088ed309e81f8
Kerriea-star <ojared511@gmail.com>
2023-01-26 09:01:47 +0300

    fixes

--- iris/settings.py
+++ iris/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'IrisDB',
         'USER': "postgres",
-        'PASSWORD': "68@259779Ja",
+        'PASSWORD': "Ja@259779",
         "HOST": "127.0.0.1",
         'PORT': '5432'
     }
