482d7bd4515e9340b8abe9de0b6f1c9933b0a6ed
Kerriea-star <ojared511@gmail.com>
2023-01-31 11:14:26 +0300

    changes

--- iris/settings.py
+++ iris/settings.py
@@ -68,11 +68,11 @@ WSGI_APPLICATION = 'iris.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env ("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        'PORT': env('DB_PORT')
+        'NAME': "IrisDB",
+        'USER': "postgres",
+        'PASSWORD': "Ja@259779",
+        "HOST": "localhost",
+        'PORT': "5432"
     }
 }
 
