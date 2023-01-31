8b4a6df5ba7524435ab16bd1139c57fe8d36ed4f
Kerriea-star <ojared511@gmail.com>
2023-01-31 11:33:54 +0300

    fixes

--- iris/settings.py
+++ iris/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = '*e-vifgtz8^38rhj*x%&-k_&vs+v*24g1r_+#6atve@scpwylo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
